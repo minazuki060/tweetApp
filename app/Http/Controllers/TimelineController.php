44887ffaae875be3c6af0277511bfd9bda665bbd
minazuki060 <119671496+minazuki060@users.noreply.github.com>
2023-03-20 21:51:37 +0900

    photoの登録処理

--- app/Http/Controllers/TimelineController.php
+++ app/Http/Controllers/TimelineController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use \App\Models\Tweet;
+use \App\Models\Photo;
 
 class TimelineController extends Controller
 {
@@ -33,11 +34,28 @@ class TimelineController extends Controller
         $userId = Auth::id();
 
         //tweetの登録処理（データベースに登録）
-        Tweet::create([
+        $tweet = Tweet::create([
             'text' => $request['text'],
-            //use_rid追加
+            //user_id追加
             'user_id' => $userId,
         ]);
+
+        //photoの登録処理
+         // ディレクトリ名
+         $dir = 'sample';
+
+         // アップロードされたファイル名を取得
+         $file_name = $request->file('image')->getClientOriginalName();
+
+         // sampleディレクトリに画像を保存(取得したファイル名で)
+         $request->file('image')->storeAs('public/' . $dir , $file_name);
+
+         //データベースへ保存
+         $photo = Photo::create([
+             'file_name' => $file_name,
+             'tweet_id' => $tweet->id,
+        ]);
+        
 
         //showTimelinePageを実行
         $this->showTimelinePage();
